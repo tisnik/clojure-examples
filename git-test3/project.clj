b3356c7761e27c39619eda5cd5dd4388c436388e
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-06-21 17:26:19 +0200

    Added new example: git-test3

---
+++ git-test3/project.clj
@@ -0,0 +1,11 @@
+(defproject git-test3 "0.1.0-SNAPSHOT"
+    :description "FIXME: write description"
+    :url "http://example.com/FIXME"
+    :license {:name "Eclipse Public License"
+              :url "http://www.eclipse.org/legal/epl-v10.html"}
+    :dependencies [[org.clojure/clojure "1.6.0"]
+                   [clj-jgit "0.8.0"]
+                   [clj-rm-rf "1.0.0-SNAPSHOT"]]
+    :main ^:skip-aot git-test3.core
+    :target-path "target/%s"
+    :profiles {:uberjar {:aot :all}})
