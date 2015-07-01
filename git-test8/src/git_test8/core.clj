8786668c9dda82a0dcdeb6d6fdc7383c72739fe0
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-07-01 20:01:22 +0200

    Added new example: git-test8

---
+++ git-test8/src/git_test8/core.clj
@@ -0,0 +1,69 @@
+(ns git-test8.core
+    (:gen-class))
+
+(require '[clj-jgit.porcelain :as jgit])
+(require '[clj-jgit.querying  :as jgit-query])
+(require '[hozumi.rm-rf :as rm-rf])
+
+(def repository-url
+    "Adresa GIT repositare vcetne specifikace protokolu."
+    "https://github.com/tisnik/testrepo.git")
+
+(def directory-name
+    "Jmeno adresare, do ktereho se GIT repositar naklonuje."
+    "repo")
+
+(defn clone-repository
+    "Naklonovani GIT repositare do specifikovaneho adresare."
+    [url directory]
+    (jgit/git-clone url directory))
+
+(defn delete-directory
+    "Smazani adresare vcetne podadresaru a souboru."
+    [directory]
+    (rm-rf/rm-r (java.io.File. directory)))
+
+(defn print-customized-log
+    "Vypis logovacich informaci ve zvolenem formatu."
+    [repository]
+    (println "\nLog:")
+    (doseq [log-entry (jgit/git-log repository)]
+        (let [info (jgit-query/commit-info repository log-entry)]
+             (println (:author info) ":" (:message info)))))
+
+(defn print-local-branches
+    "Vypis vsech lokalnich vetvi pro vybrany repositar."
+    [repo]
+    (println "\nLocal branches")
+    (doseq [branch (jgit/git-branch-list repo)]
+        (println (.getName branch))))
+
+(defn print-remote-branches
+    "Vypis vsech vzdalenych vetvi pro vybrany repositar."
+    [repo]
+    (println "\nRemote branches")
+    (doseq [branch (jgit/git-branch-list repo :remote)]
+        (println (.getName branch))))
+
+(defn git-test-8
+    "Naklonovani repositare, nacteni informaci
+     z vytvoreneho adresare a vypis podrobnejsiho logu."
+    [repository-url directory-name]
+    ; naklonovani repositare do specifikovaneho adresare
+    (clone-repository repository-url directory-name)
+    ; nacteni informaci o repositari z lokalniho adresare
+    ; povsimnete si pouziti "automagicky" vytvorene promenne,
+    ; ktera se jmenuje 'repo'
+    (try (jgit/with-repo directory-name
+                         (print-customized-log repo)
+                         (print-local-branches repo)
+                         (print-remote-branches repo))
+         (finally
+             ; vymazani adresare s naklonovanym repositarem
+             (delete-directory directory-name))))
+
+(defn -main
+    "Funkce zavolana po zadani prikazu 'lein run'."
+    [& args]
+    (git-test-8 repository-url directory-name))
+
---
+++ git-test8/test/git_test8/core_test.clj
@@ -0,0 +1,7 @@
+(ns git-test8.core-test
+  (:require [clojure.test :refer :all]
+            [git-test8.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
