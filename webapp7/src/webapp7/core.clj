0df118b69bb76d6c55a674bfa59c317d99e75c97
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-07-14 09:57:08 +0200

    New example: webapp7

---
+++ webapp7/src/webapp7/core.clj
@@ -0,0 +1,36 @@
+(ns webapp7.core
+    (:gen-class))
+
+(require '[ring.adapter.jetty     :as jetty])
+(require '[ring.middleware.params :as http-params])
+(require '[ring.util.response     :as response])
+
+(def html-page
+"    <html>
+         <head>
+             <title>Powered by Ring!</title>
+         </head>
+         <body>
+             <h1>Powered by Ring!</h1>
+         </body>
+     </html>
+")
+
+(defn handler
+    "Zpracovani pozadavku."
+    [request]
+    (let [params (:params request)]
+        (println "Params: " params))
+    (-> (response/response html-page)
+        (response/content-type "text/html; charset=utf-8")))
+
+(def app
+    "Datova struktura predstavujici kostru webove aplikace."
+    (-> handler
+        http-params/wrap-params))
+
+(defn -main
+    "Spusteni webove aplikace na portu 8080."
+    [& args]
+    (jetty/run-jetty app {:port 8080}))
+
---
+++ webapp7/test/webapp7/core_test.clj
@@ -0,0 +1,7 @@
+(ns webapp7.core-test
+  (:require [clojure.test :refer :all]
+            [webapp7.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
