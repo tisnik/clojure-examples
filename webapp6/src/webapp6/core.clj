8841e9f90753171a1ec94cbfa50360e613832ed3
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-07-14 09:56:56 +0200

    New example: webapp6

---
+++ webapp6/src/webapp6/core.clj
@@ -0,0 +1,32 @@
+(ns webapp6.core
+    (:gen-class))
+
+(require '[ring.adapter.jetty     :as jetty])
+(require '[ring.util.response     :as response])
+
+(def html-page
+"    <html>
+         <head>
+             <title>Powered by Ring!</title>
+         </head>
+         <body>
+             <h1>Powered by Ring!</h1>
+         </body>
+     </html>
+")
+
+(defn handler
+    "Zpracovani pozadavku."
+    [request]
+    (-> (response/response html-page)
+        (response/content-type "text/html; charset=utf-8")))
+
+(def app
+    "Datova struktura predstavujici kostru webove aplikace."
+    (-> handler))
+
+(defn -main
+    "Spusteni webove aplikace na portu 8080."
+    [& args]
+    (jetty/run-jetty app {:port 8080}))
+
