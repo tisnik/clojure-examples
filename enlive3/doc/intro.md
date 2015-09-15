40c9a565b94059749380b8a9c57bf13c8cba3526
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-09-15 19:26:36 +0200

    3rd test for Enlive library

---
+++ enlive3/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to enlive3
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ enlive3/src/enlive3/core.clj
@@ -0,0 +1,38 @@
+(ns enlive3.core
+    (:gen-class))
+
+(require '[net.cgrand.enlive-html :as html])
+
+(def roles [
+    {:actor-name "Zdeněk Svěrák"   :character "inspektor Trachta  "}
+    {:actor-name "Petr Brukner"    :character "praktikant Hlaváček"}
+    {:actor-name "Miloň Čepelka"   :character "praktikant Hlaváček"}
+    {:actor-name "Bořivoj Penc"    :character "továrník Bierhanzel"}
+    {:actor-name "Jaroslav Weigel" :character "továrník Bierhanzel"}
+    {:actor-name "Jan Hraběta"     :character "továrník Meyer"}
+    {:actor-name "Václav Kotek"    :character "steward"}
+    {:actor-name "Genadij Rumlena" :character "steward"}])
+
+(def vrazda-v-salonnim-coupe
+    {:title "Vražda v salonním coupé"
+     :roles roles
+    })
+
+(html/defsnippet one-record "test.html"
+    {[:div#role]     ; zacatek
+     [:div#role]}    ; konec
+    [record]
+    [:span#actor]     (html/content (:actor-name record))
+    [:span#character] (html/content (:character record)))
+
+(html/deftemplate test-page "test.html"
+    [data-for-page]
+    [:title]      (html/content (:title data-for-page))
+    [:h1 :span]   (html/content (:title data-for-page))
+    [:div#roles]  (html/content (map one-record (:roles data-for-page))) ; vnitrek odstavce bude duplikovan
+)
+
+(defn -main
+    [& args]
+    (println (reduce str (test-page vrazda-v-salonnim-coupe))))
+
