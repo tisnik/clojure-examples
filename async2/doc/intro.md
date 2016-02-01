789f49533a64c5aeeff371d0c285336bf9fd2589
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-02-01 09:19:49 +0100

    core.async example #2

---
+++ async2/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to async2
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ async2/src/async2/core.clj
@@ -0,0 +1,38 @@
+(ns async2.core
+    (:gen-class))
+
+; nacteme vsechny potrebne funkce, makra a symboly z knihovny
+; (schvalne se nenacitaji vsechny funkce, protoze by jejich jmena
+;  prepsala takove zakladni funkce, jako je map apod.)
+(require '[clojure.core.async :refer (go chan >! <! timeout)])
+
+(defn wait
+    "Pozastaveni hlavniho vlakna - simulace interaktivni prace."
+    []
+    (Thread/sleep 15000))
+
+(defn -main
+    "Tato funkce se spusti automaticky nastrojem Leiningen."
+    [& args]
+    (println "Start")
+    ; vytvorime kanal
+    (let [channel (chan)]
+        ; kontinualni posilani zprav do kanalu v asynchronnim bloku
+        (go
+            (dotimes [i 10]
+                ;(Thread/sleep 1000)
+                (<! (timeout 1000))
+                (>! channel i)))
+
+        (println "1st go block started")
+
+        ; kontinualni cteni zprav z kanalu v asynchronnim bloku
+        (go
+            (while true
+                (println (<! channel))))
+
+        (println "2nd go block started"))
+    ; chvili pockame, az se vypise cela sekvence 0 az 9
+    (wait)
+    (println "Finish"))
+
