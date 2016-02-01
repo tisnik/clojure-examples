a52bfa2e7717c86a8af36ad1596ad0cc3fa00479
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-02-01 09:24:11 +0100

    Project that use Clojure 1.8.0

---
+++ clojure8-test/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure8-test
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ clojure8-test/src/clojure8_test/core.clj
@@ -0,0 +1,7 @@
+(ns clojure8-test.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
