9bbe47d3acc2dcccc22dd24bc4139aa2ea35e6fc
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-02-12 22:09:43 +0100

    IRC bot version #4

---
+++ ircbot4/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ircbot4
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ ircbot4/test/ircbot4/core_test.clj
@@ -0,0 +1,7 @@
+(ns ircbot4.core-test
+  (:require [clojure.test :refer :all]
+            [ircbot4.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
