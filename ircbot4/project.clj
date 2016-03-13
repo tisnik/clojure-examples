a5e1977bd9cdb8dd0ac2c4e316ceff6eff3965ba
Pavel Tisnovsky <ptisnovs@redhat.com>
2016-03-13 19:04:05 +0100

    Added dependencies to the project file

--- ircbot4/project.clj
+++ ircbot4/project.clj
@@ -1,10 +1,16 @@
 (defproject ircbot4 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [irclj "0.5.0-alpha4"]]
-  :main ^:skip-aot ircbot4.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+    :description "FIXME: write description"
+    :url "http://example.com/FIXME"
+    :license {:name "Eclipse Public License"
+              :url "http://www.eclipse.org/legal/epl-v10.html"}
+    :dependencies [[org.clojure/clojure "1.7.0"]
+                   [irclj "0.5.0-alpha4"]
+                   [clojure-ini "0.0.1"]]
+    :main ^:skip-aot ircbot4.core
+    :target-path "target/%s"
+    :plugins [[lein-ring "0.8.10"]
+              [codox "0.8.11"]
+              [test2junit "1.1.0"]
+              [lein-cloverage "1.0.6"]]
+    :profiles {:uberjar {:aot :all}})
+
