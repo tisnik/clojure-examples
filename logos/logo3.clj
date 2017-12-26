a4870afb18d77de61a9b334fe471d153234900e6
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-12-26 21:29:26 +0100

    Logo, 3rd version

---
+++ logos/logo3.clj
@@ -0,0 +1,7 @@
+(spit"logo.svg"(str"<svg xmlns='http://www.w3.org/2000/svg' width='480'
+version='1.1' height='480'>"(loop[i 0 R 255 G 255 B 0 o ""](let[r(- 128
+i)q(str"fill='rgb("R","G","B")' style='fill-opacity:.06'/>")a(/ i 12.
+)b(+ i 80)x(+ 240(* b(Math/cos a)))y(+ 240(* b(Math/sin a)))p(format
+"<circle cx='%f' cy='%f' r='%d' "x y r)](if(< i 128)(recur(inc i)(- R
+2)G(+ B 2)(str o p q p"fill='none' stroke='black'/>"))o)))"</svg>"))
+
