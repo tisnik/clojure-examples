86daa676ef52396d7c74065381de3e6414aa3820
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-03-01 09:08:55 +0100

    The 'spec' library: fifth demo

---
+++ spec-demo5/src/spec_demo5/core.clj
@@ -0,0 +1,54 @@
+(ns spec-demo5.core)
+
+(require '[clojure.spec.alpha :as spec])
+
+(defn name?
+    [s]
+    (and (string? s) (re-matches #"[A-Z][a-z]+" s)))
+
+(defn surname?
+    [s]
+    (and (string? s) (re-matches #"[A-Z][A-Za-z ]+" s)))
+
+(defn email?
+    [s]
+    (let [pattern #"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"]
+        (and (string? s) (re-matches pattern s))))
+
+(spec/def ::id      pos-int?)
+(spec/def ::phone   pos-int?)
+(spec/def ::name    name?)
+(spec/def ::surname surname?)
+(spec/def ::e-mail   email?)
+(spec/def ::person? (spec/keys :req-un [::id ::name ::surname] :opt-un [::phone ::e-mail]))
+
+(defn -main
+    "I don't do a whole lot ... yet."
+    [& args]
+    (let [persons [ {:id 10 :name "Rich" :surname "Hickey"}
+                    {:id 10 :name "rich" :surname "Hickey"}
+                    {:id 10 :name "Rich" :surname "hickey"}
+                    {:id -10 :name "Rich" :surname "Hickey"}
+                    {:id -10 :name "rich" :surname "hickey"}
+                    {:id 10 :name "Rich" :surname "Hickey" :e-mail "rich@somewhere.org"}
+                    {:id 10 :name "Rich" :surname "Hickey" :e-mail "wrong"}
+                    {:id 10 :name "Rich" :surname "Hickey" :phone 123456789}
+                    {:id 10 :name "Rich" :surname "Hickey" :phone nil}
+                    {:id 10 :name "Rich" :surname "Hickey" :phone 987654321 :e-mail "rich@somewhere.org"}
+                    {:id -10 :name "rich" :surname "" :phone -5 :e-mail "wrong"}]]
+        (println "valid?")
+        (println "-------------------------------")
+
+        (doseq [person persons]
+            (println person)
+            (println (if (spec/valid? ::person? person) "yes\n" "no\n")))
+
+        (println "\n\n\n")
+        (println "explain")
+        (println "-------------------------------")
+
+        (doseq [person persons]
+            (println person)
+            (println (spec/explain ::person? person))
+
+            (println))))
