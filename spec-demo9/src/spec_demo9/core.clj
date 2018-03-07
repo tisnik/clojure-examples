ad2eef819294affef5e23e918cd080e3f6eb73d2
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-03-07 14:17:12 +0100

    Implementation of spec demo #9

--- spec-demo9/src/spec_demo9/core.clj
+++ spec-demo9/src/spec_demo9/core.clj
@@ -1,7 +1,45 @@
-(ns spec-demo9.core
-  (:gen-class))
+(ns spec-demo9.core)
+
+(require '[clojure.spec.alpha :as spec])
+
+(spec/def ::comment (spec/nilable string?))
+
+
+(defn hr
+    []
+    (println "-------------------------------"))
+
+(defn print-title
+    [title]
+    (hr)
+    (println title)
+    (hr)
+    (println))
+
+
+(defn validate-comment
+    [comment-value]
+    (println "comment=" comment-value)
+    (println (if (spec/valid? ::comment comment-value) "valid" "invalid"))
+    (println))
+
+
+(defn explain-comment-validation
+    [comment-value]
+    (println "comment-value" comment-value)
+    (println (spec/explain ::comment comment-value))
+    (println))
+
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+    [& args]
+    (let [comments [nil 42 [] "text" "text\non\nfour\nlines"]]
+         (print-title "Validation")
+
+         (doseq [comment-value comments]
+             (validate-comment comment-value))
+
+         (print-title "Explain validation failures")
+
+         (doseq [comment-value comments]
+             (explain-comment-validation comment-value))))
