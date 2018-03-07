8368594d85791ebb9def3cfbdfb1a630f4723707
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-03-07 12:20:59 +0100

    Implementation of spec demo #7

--- spec-demo7/src/spec_demo7/core.clj
+++ spec-demo7/src/spec_demo7/core.clj
@@ -1,7 +1,45 @@
-(ns spec-demo7.core
-  (:gen-class))
+(ns spec-demo7.core)
+
+(require '[clojure.spec.alpha :as spec])
+
+(spec/def ::unique-id (spec/or :name string? :id pos-int?))
+
+
+(defn hr
+    []
+    (println "-------------------------------"))
+
+(defn print-title
+    [title]
+    (hr)
+    (println title)
+    (hr)
+    (println))
+
+
+(defn validate-unique-id
+    [id]
+    (println "id=" id)
+    (println (if (spec/valid? ::unique-id id) "valid" "invalid"))
+    (println))
+
+
+(defn explain-unique-id-validation
+    [id]
+    (println "id=" id)
+    (println (spec/explain ::unique-id id))
+    (println))
+
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+    [& args]
+    (let [ids [nil [] 1/2 0.5 -1 "42" 0 "" 1 1000 10000 1000000000 10000000000 "*?"]]
+         (print-title "Validation")
+
+         (doseq [id ids]
+             (validate-unique-id id))
+
+         (print-title "Explain validation failures")
+
+         (doseq [id ids]
+             (explain-unique-id-validation id))))
