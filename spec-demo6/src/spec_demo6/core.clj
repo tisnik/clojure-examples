942adc82cf6e8af9d36e2e02b70bda8d7a9e1544
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-03-07 12:03:34 +0100

    Implementation of spec demo #6

--- spec-demo6/src/spec_demo6/core.clj
+++ spec-demo6/src/spec_demo6/core.clj
@@ -1,7 +1,45 @@
-(ns spec-demo6.core
-  (:gen-class))
+(ns spec-demo6.core)
+
+(require '[clojure.spec.alpha :as spec])
+
+(spec/def ::user-id (spec/and pos-int? #(>= % 1000) #(< % Integer/MAX_VALUE)))
+
+
+(defn hr
+    []
+    (println "-------------------------------"))
+
+(defn print-title
+    [title]
+    (hr)
+    (println title)
+    (hr)
+    (println))
+
+
+(defn validate-user-id
+    [id]
+    (println "id=" id)
+    (println (if (spec/valid? ::user-id id) "valid" "invalid"))
+    (println))
+
+
+(defn explain-user-id-validation
+    [id]
+    (println "id=" id)
+    (println (spec/explain ::user-id id))
+    (println))
+
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+    [& args]
+    (let [ids [-1 0 1 1000 10000 1000000000 10000000000]]
+         (print-title "Validation")
+
+         (doseq [id ids]
+             (validate-user-id id))
+
+         (print-title "Explain validation failures")
+
+         (doseq [id ids]
+             (explain-user-id-validation id))))
