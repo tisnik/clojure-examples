d8cc0f29db69e35dfaa207d4a9971da62893b62d
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-03-20 09:34:49 +0100

    Cucumber+expect demo #3 (WIP)

---
+++ cucumber+expect3/test/cucumber+expect3/core_test.clj
@@ -0,0 +1,7 @@
+(ns cucumber+expect3.core-test
+  (:require [clojure.test :refer :all]
+            [cucumber+expect3.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
