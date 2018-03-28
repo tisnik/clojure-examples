09ed0fe4a338bffa1f4b50494e4cad5713cd5035
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-03-28 10:28:48 +0200

    Updated project name

--- cucumber+expectB/project.clj
+++ cucumber+expectB/project.clj
@@ -1,4 +1,4 @@
-(defproject cucumber+expectA "0.1.0-SNAPSHOT"
+(defproject cucumber+expectB "0.1.0-SNAPSHOT"
     :description "FIXME: write description"
     :url "http://example.com/FIXME"
     :license {:name "Eclipse Public License"
