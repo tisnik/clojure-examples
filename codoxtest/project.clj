8fec5b7d4d9ee6ae9e2200ec7512bfd97157627d
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-17 10:09:29 +0200

    Bump up Clojure version

--- codoxtest/project.clj
+++ codoxtest/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot codoxtest.core
   :target-path "target/%s"
   :plugins [[codox "0.8.11"]]
