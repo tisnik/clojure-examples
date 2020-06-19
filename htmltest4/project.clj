5b32c5aab209e2ed5e62c8758bb127519b36e856
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-19 08:59:36 +0200

    Bump up Clojure version to 1.8.0

--- htmltest4/project.clj
+++ htmltest4/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [hiccup "1.0.4"]]
   :main ^:skip-aot htmltest4.core
   :target-path "target/%s"
