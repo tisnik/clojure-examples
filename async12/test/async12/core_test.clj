4d1a4b1ece7136d350c1390a2bc40994a08f237b
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-24 08:07:50 +0200

    Added missing (c) header

--- async12/test/async12/core_test.clj
+++ async12/test/async12/core_test.clj
@@ -1,3 +1,15 @@
+;
+;  (C) Copyright 2016, 2020  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
 (ns async12.core-test
   (:require [clojure.test :refer :all]
             [async12.core :refer :all]))
