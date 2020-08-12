9bdc8c863143e0d72a0b7912f2ced30e48426b1a
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-08-12 15:09:58 +0200

    Example #22: sequential map

---
+++ babashka/22_sequential_map.clj
@@ -0,0 +1,14 @@
+(defn compute-pi
+  ([n pi]
+  (loop [i 3
+         pi pi]
+    (if (<= i (+ n 2))
+      (recur (+ i 2) (* pi (/ (- i 1) i) (/ (+ i 1) i)))
+      pi)))
+  ([n]
+   (compute-pi n 4.0)))
+
+(let [n (range 1000000 1000020)
+      results (doall (map #(compute-pi %) n))]
+  (doseq [pi (doall results)]
+    (println pi)))
