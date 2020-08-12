43ec36e71d2646835d08eb1fff42d6eab99c1e7a
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-08-12 17:21:37 +0200

    Example #24: pmap usage for stock Clojure

---
+++ babashka/24_parallel_map_clojure.clj
@@ -0,0 +1,16 @@
+(defn compute-pi
+  ([n pi]
+  (loop [i 3
+         pi pi]
+    (if (<= i (+ n 2))
+      (recur (+ i 2) (* pi (/ (- i 1) i) (/ (+ i 1) i)))
+      pi)))
+  ([n]
+   (compute-pi n 4.0)))
+
+(let [n (range 1000000 1000020)
+      results (doall (pmap #(compute-pi %) n))]
+  (doseq [pi results]
+    (println pi)))
+
+(System/exit 0)
