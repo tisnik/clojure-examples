6f9357321727f91e1732d68b9ec82f4cf4e9f22f
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-08-12 09:11:45 +0200

    Example 13: CLI arguments parsing

---
+++ babashka/13_cli_arguments.clj
@@ -0,0 +1,17 @@
+#!/usr/bin/env bb
+
+(require '[clojure.pprint :as pprint])
+(require '[clojure.tools.cli :refer [parse-opts]])
+
+(def command-line-options
+  [["-v" "--verbose" "Verbosity level"
+    :id :verbosity
+    :default 0
+    :update-fn inc]
+   ["-p" "--port PORT" "Port number"
+    :default 80
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
+   ["-h" "--help"]])
+
+(pprint/pprint (parse-opts *command-line-args* command-line-options))
