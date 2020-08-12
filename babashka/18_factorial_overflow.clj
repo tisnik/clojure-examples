4e299500c56207a0564127c9f38b4c668b755c94
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-08-12 09:11:45 +0200

    Example 18: factorial computation with overflow

---
+++ babashka/18_factorial_overflow.clj
@@ -0,0 +1,12 @@
+(defn factorial
+    [n]
+    (if (neg? n)
+        (throw (IllegalArgumentException. "negative numbers are not supported!"))
+        (apply * (range 1 (inc n)))))
+
+(defn main
+    [max]
+    (doseq [i (range 0 (inc max))]
+        (println i "! = " (factorial i))))
+
+(main 50)
