f29502f9bbd80115d876ebd898ba2e286f8eea58
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-08-12 09:11:45 +0200

    Example 20: Pi computation with using double arithmetic

---
+++ babashka/20_pi_computation_double.clj
@@ -0,0 +1,14 @@
+(defn compute-pi
+  ([n pi]
+  (loop [i 3
+         pi pi]
+    (if (<= i (+ n 2))
+      (recur (+ i 2) (* pi (/ (- i 1) i) (/ (+ i 1) i)))
+      pi)))
+  ([n]
+   (compute-pi n 4.0)))
+
+
+(doseq [i (range 0 20)]
+  (let [n (bit-shift-left 1 i)]
+    (println n "\t" (compute-pi n))))
