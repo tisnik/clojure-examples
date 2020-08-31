2d63c08e9bcf195116dc9d363a53b7a4d2b41860
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-08-31 09:50:55 +0200

    Bump up Clojure version

--- test-clj-flotr/project.clj
+++ test-clj-flotr/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojars.tisnik/clj-flotr "0.1.0-SNAPSHOT"]]
   :main ^:skip-aot test-clj-flotr.core
   :target-path "target/%s"
