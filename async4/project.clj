cba689f1113ea9ec3ac8267ae222f3fd00377549
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-01-07 09:22:12 +0100

    Bump up Clojure version to 10 in project async4

--- async4/project.clj
+++ async4/project.clj
@@ -1,5 +1,5 @@
 ;
-;  (C) Copyright 2016, 2020  Pavel Tisnovsky
+;  (C) Copyright 2016, 2020, 2021  Pavel Tisnovsky
 ;
 ;  All rights reserved. This program and the accompanying materials
 ;  are made available under the terms of the Eclipse Public License v1.0
@@ -13,9 +13,9 @@
 (defproject async4 "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
+  :license {:name "Eclipse Public License",
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/core.async "0.2.374"]]
   :main ^:skip-aot async4.core
   :target-path "target/%s"
