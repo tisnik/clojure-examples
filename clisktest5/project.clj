4b3816345ec3ef42f11209983e3ec139e3c9ae25
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-08 10:20:29 +0100

    Bump-up Clojure version in clisktest5

--- clisktest5/project.clj
+++ clisktest5/project.clj
@@ -1,9 +1,21 @@
+;
+;  (C) Copyright 2015, 2020, 2021  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
 (defproject clisktest5 "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [net.mikera/clisk "0.10.0"]]
   :main ^:skip-aot clisktest5.core
   :target-path "target/%s"
