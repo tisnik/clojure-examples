53891cd872daccd875e653e6c731ea872131282b
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-19 09:09:47 +0100

    Bump-up Clojure version for project humane-output

--- humane-output/project.clj
+++ humane-output/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [ring/ring-core "1.3.2"]]
   :main ^:skip-aot humane-output.core
   :target-path "target/%s"
