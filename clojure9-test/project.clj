1965065ecc8f35aace88a91d0900f6f9fff433b0
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-19 09:09:21 +0100

    Bump-up Clojure version for project clojure9-test

--- clojure9-test/project.clj
+++ clojure9-test/project.clj
@@ -1,9 +1,9 @@
 (defproject clojure9-test "0.1.0-SNAPSHOT"
-  :description "Projekt, který zajistí stažení Clojure verze 1.9.0"
+  :description "Projekt, který zajistí stažení Clojure verze 1.10.1"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]]
   :main ^:skip-aot clojure9-test.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
