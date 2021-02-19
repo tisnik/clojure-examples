48027c2316f16811dc6e8d9986b2f55a0ab600ab
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-19 09:09:21 +0100

    Bump-up Clojure version for project cloverage

--- cloverage/project.clj
+++ cloverage/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]]
   :main ^:skip-aot cloverage.core
   :target-path "target/%s"
   :plugins [[lein-cloverage "1.0.2"]]
