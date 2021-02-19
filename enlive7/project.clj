05c17518a188a26aaff230327ab9b452a8debc96
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-19 09:09:21 +0100

    Bump-up Clojure version for project enlive7

--- enlive7/project.clj
+++ enlive7/project.clj
@@ -15,7 +15,7 @@
     :url "http://example.com/FIXME"
     :license {:name "Eclipse Public License"
               :url "http://www.eclipse.org/legal/epl-v10.html"}
-    :dependencies [[org.clojure/clojure "1.8.0"]
+    :dependencies [[org.clojure/clojure "1.10.1"]
                    [enlive "1.1.1"]]
     :main ^:skip-aot enlive7.core
     :target-path "target/%s"
