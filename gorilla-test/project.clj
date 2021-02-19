12a0617d01906908ed71e0f60345eca766845838
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-19 09:09:47 +0100

    Bump-up Clojure version for project gorilla-test

--- gorilla-test/project.clj
+++ gorilla-test/project.clj
@@ -15,7 +15,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]]
   :main ^:skip-aot gorilla-test.core
   :target-path "target/%s"
   :plugins [[lein-gorilla "0.3.6"]]
