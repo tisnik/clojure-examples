d21ef5745040368615f4d31b267869eca1c8962d
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-19 09:09:21 +0100

    Bump-up Clojure version for project factorial3

--- factorial3/project.clj
+++ factorial3/project.clj
@@ -15,7 +15,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [expectations "2.0.9"]]
   :main ^:skip-aot factorial2.core
   :target-path "target/%s"
