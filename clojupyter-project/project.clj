2b0e42162833fa92306c55600f34b1321e7cbb48
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-19 09:09:21 +0100

    Bump-up Clojure version for project clojupyter-project

--- clojupyter-project/project.clj
+++ clojupyter-project/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [incanter "1.9.3"]]
   :main ^:skip-aot xyz.core
   :target-path "target/%s"
