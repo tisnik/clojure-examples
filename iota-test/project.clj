e2cc5b2af5a3634a66614ef759b80ec2fd287484
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-19 09:09:47 +0100

    Bump-up Clojure version for project iota-test

--- iota-test/project.clj
+++ iota-test/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [juxt/iota "0.2.3"]
                  [ring/ring-core "1.3.2"]]
   :main ^:skip-aot iota-test.core
