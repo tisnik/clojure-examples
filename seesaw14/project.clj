fc744a4335f1cd3f8e1d8854d6ce6032e37bea9e
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-19 09:10:13 +0100

    Bump-up Clojure version for project seesaw14

--- seesaw14/project.clj
+++ seesaw14/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [seesaw "1.4.5"]]
   :main ^:skip-aot seesaw14.core
   :target-path "target/%s"
