43c634ead4e71b251ba74eadfa899ee6650e5852
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-19 09:09:47 +0100

    Bump-up Clojure version for project htmltest4

--- htmltest4/project.clj
+++ htmltest4/project.clj
@@ -15,7 +15,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [hiccup "1.0.4"]]
   :main ^:skip-aot htmltest4.core
   :target-path "target/%s"
