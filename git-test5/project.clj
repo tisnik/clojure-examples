fb42180d80a9a741e2d5aedb89643ec0aa4cdcd9
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-19 09:09:47 +0100

    Bump-up Clojure version for project git-test5

--- git-test5/project.clj
+++ git-test5/project.clj
@@ -3,7 +3,7 @@
     :url "http://example.com/FIXME"
     :license {:name "Eclipse Public License"
               :url "http://www.eclipse.org/legal/epl-v10.html"}
-    :dependencies [[org.clojure/clojure "1.6.0"]
+    :dependencies [[org.clojure/clojure "1.10.1"]
                    [clj-jgit "0.8.0"]
                    [clj-rm-rf "1.0.0-SNAPSHOT"]]
     :main ^:skip-aot git-test5.core
