b7e0e55c4fb5ee1ce476f8728c1befa9fa3439f9
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-22 08:59:35 +0100

    Bump-up Clojure version for project sqltest3

--- sqltest3/project.clj
+++ sqltest3/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/java.jdbc "0.3.5"]
                  [org.xerial/sqlite-jdbc "3.7.2"]]
   :main ^:skip-aot sqltest3.core
