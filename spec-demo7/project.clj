750822a29199601247a24f77a8f0708983cf7349
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-22 08:59:34 +0100

    Bump-up Clojure version for project spec-demo7

--- spec-demo7/project.clj
+++ spec-demo7/project.clj
@@ -15,7 +15,7 @@
     :url "http://example.com/FIXME"
     :license {:name "Eclipse Public License"
               :url "http://www.eclipse.org/legal/epl-v10.html"}
-    :dependencies [[org.clojure/clojure "1.9.0"]]
+    :dependencies [[org.clojure/clojure "1.10.1"]]
     :main ^:skip-aot spec-demo7.core
     :target-path "target/%s"
     :profiles {:uberjar {:aot :all}})
