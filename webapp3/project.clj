42b461c8652d319bff57df49ea6d0b323b0d7605
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-22 08:59:35 +0100

    Bump-up Clojure version for project webapp3

--- webapp3/project.clj
+++ webapp3/project.clj
@@ -15,7 +15,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [ring/ring-core "1.3.2"]
                  [ring/ring-jetty-adapter "1.3.2"]]
   :main ^:skip-aot webapp3.core
