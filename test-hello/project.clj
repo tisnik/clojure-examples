4a1c032a8441e380b8900787bf6753ca6f1c0861
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-22 08:59:35 +0100

    Bump-up Clojure version for project test-hello

--- test-hello/project.clj
+++ test-hello/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojars.tisnik/clj-hello "0.1.0-SNAPSHOT"]]
   :main ^:skip-aot test-hello.core
   :target-path "target/%s"
