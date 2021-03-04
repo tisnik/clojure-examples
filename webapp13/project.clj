71edb943b604471b2f9544c8ba55973d1eed542f
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-03-04 09:17:07 +0100

    Added new plugin lein-codox into webapp13/project.clj

--- webapp13/project.clj
+++ webapp13/project.clj
@@ -19,6 +19,8 @@
                  [ring/ring-core "1.3.2"]
                  [ring/ring-jetty-adapter "1.3.2"]
                  [hiccup "1.0.4"]]
+  :plugins [[lein-codox "0.10.7"]
+            ]
   :main ^:skip-aot webapp13.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
