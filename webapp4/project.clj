243b5f5751f7602aa80da941e7c3c4cd943be761
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-03-11 07:59:00 +0100

    Added new dependency lein-clean-m2 into webapp4

--- webapp4/project.clj
+++ webapp4/project.clj
@@ -1,5 +1,5 @@
 ;
-;  (C) Copyright 2015, 2020  Pavel Tisnovsky
+;  (C) Copyright 2015, 2020, 2021  Pavel Tisnovsky
 ;
 ;  All rights reserved. This program and the accompanying materials
 ;  are made available under the terms of the Eclipse Public License v1.0
@@ -23,6 +23,7 @@
             ;[lein-test-out "0.3.1"]
             [lein-cloverage "1.0.7-SNAPSHOT"]
             [lein-kibit "0.1.8"]
+            [lein-clean-m2 "0.1.2"]
             ]
   :main ^:skip-aot webapp4.core
   :target-path "target/%s"
