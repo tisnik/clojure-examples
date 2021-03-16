3e90ab95d9d69d0a1841e02b0053fd70eebed0e4
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-03-16 08:32:05 +0100

    Reformatted project file cucumber+expect1

--- cucumber+expect1/project.clj
+++ cucumber+expect1/project.clj
@@ -1,5 +1,5 @@
 ;
-;  (C) Copyright 2018, 2020  Pavel Tisnovsky
+;  (C) Copyright 2018, 2020, 2021  Pavel Tisnovsky
 ;
 ;  All rights reserved. This program and the accompanying materials
 ;  are made available under the terms of the Eclipse Public License v1.0
@@ -11,16 +11,17 @@
 ;
 
 (defproject cucumber+expect1 "0.1.0-SNAPSHOT"
-    :description "FIXME: write description"
-    :url "http://example.com/FIXME"
-    :license {:name "Eclipse Public License"
-              :url "http://www.eclipse.org/legal/epl-v10.html"}
-    :dependencies [[org.clojure/clojure "1.10.1"]
-                   [expectations "2.0.9"]]
-    :plugins [[com.siili/lein-cucumber "1.0.7"]
-              [lein-expectations "0.0.8"]]
-    :cucumber-feature-paths ["test/features/"]
-    :main ^:skip-aot cucumber+expect1.core
-    :target-path "target/%s"
-    :profiles {:uberjar {:aot :all}
-               :dev {:dependencies [[com.siili/lein-cucumber "1.0.7"]]}})
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [expectations "2.0.9"]]
+  :plugins [[com.siili/lein-cucumber "1.0.7"]
+            [lein-expectations "0.0.8"]
+            ]
+  :cucumber-feature-paths ["test/features/"]
+  :main ^:skip-aot cucumber+expect1.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}
+             :dev {:dependencies [[com.siili/lein-cucumber "1.0.7"]]}})
