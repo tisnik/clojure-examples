c40b7aef88df65e4d97edd703e0729f99995755b
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-03-17 08:50:59 +0100

    Added new dependency lein-kibit to project clisktest4

--- clisktest4/project.clj
+++ clisktest4/project.clj
@@ -21,6 +21,7 @@
             [test2junit "1.1.0"]
             ;[lein-test-out "0.3.1"]
             [lein-cloverage "1.0.7-SNAPSHOT"]
+            [lein-kibit "0.1.8"]
             ]
   :main ^:skip-aot clisktest4.core
   :target-path "target/%s"
