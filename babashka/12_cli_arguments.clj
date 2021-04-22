cf12d77e9125363466c5a32af29ec95363194810
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-04-22 11:54:31 +0200

    Added license header into 12_cli_arguments.clj

--- babashka/12_cli_arguments.clj
+++ babashka/12_cli_arguments.clj
@@ -1,3 +1,17 @@
+#!/usr/local/bin/bb
+
+;
+;  (C) Copyright 2020, 2021  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
 (require '[clojure.pprint :as pprint])
 (require '[clojure.tools.cli :refer [parse-opts]])
 
