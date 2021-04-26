c03205b9a02e02a69cc0fdf54be9b9514aba7a20
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-04-26 07:59:25 +0200

    Generated dependencies for project clicktest7

---
+++ clisktest7/doc/dependencies.clj
@@ -0,0 +1,30 @@
+{[clojure-complete "0.2.5" :exclusions [[org.clojure/clojure]]] nil,
+ [net.mikera/clisk "0.10.0"]
+ {[com.jhlabs/filters "2.0.235-1"] nil,
+  [net.mikera/clojure-utils "0.6.1"] nil,
+  [net.mikera/imagez "0.4.1"] {[org.imgscalr/imgscalr-lib "4.2"] nil},
+  [net.mikera/mathz "0.3.0"] {[net.mikera/randomz "0.3.0"] nil},
+  [net.mikera/mikera-gui "0.3.1"] nil,
+  [net.mikera/vectorz-clj "0.23.0"]
+  {[net.mikera/core.matrix "0.26.0"] nil},
+  [net.mikera/vectorz "0.38.0"] {[us.bpsm/edn-java "0.4.4"] nil},
+  [org.clojure/tools.macro "0.1.5"] nil},
+ [nrepl "0.7.0" :exclusions [[org.clojure/clojure]]] nil,
+ [org.clojure/clojure "1.10.1"]
+ {[org.clojure/core.specs.alpha "0.2.44"] nil,
+  [org.clojure/spec.alpha "0.2.176"] nil},
+ [venantius/ultra "0.6.0"]
+ {[grimradical/clj-semver "0.3.0" :exclusions [[org.clojure/clojure]]]
+  nil,
+  [io.aviso/pretty "0.1.35"] nil,
+  [mvxcvi/puget "1.1.0"]
+  {[fipp "0.6.14"] {[org.clojure/core.rrb-vector "0.0.13"] nil},
+   [mvxcvi/arrangement "1.1.1"] nil},
+  [mvxcvi/whidbey "2.1.0"] {[org.clojure/data.codec "0.1.1"] nil},
+  [org.clojars.brenton/google-diff-match-patch "0.1"] nil,
+  [robert/hooke "1.3.0"] nil,
+  [venantius/glow "0.1.5" :exclusions [[hiccup] [garden]]]
+  {[clj-antlr "0.2.3"]
+   {[org.antlr/antlr4-runtime "4.5.3"] nil,
+    [org.antlr/antlr4 "4.5.3"] nil},
+   [instaparse "1.4.1"] nil}}}
