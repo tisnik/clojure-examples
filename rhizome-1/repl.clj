1443d8fe2a02cf089c9a1f3639f83a8730101a7e
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-05-12 11:51:13 +0200

    Examples made in REPL, 3rd version

--- rhizome-1/repl.clj
+++ rhizome-1/repl.clj
@@ -12,10 +12,10 @@
 (doc viz/save-image)
 
 ; ulozeni grafu bez popisku uzlu
-(viz/save-graph (keys g) g :filename "g1.png")
+(viz/save-graph (keys g) g :filename "g01.png")
 
 ; ulozeni grafu i s popisky uzlu
-(viz/save-graph (keys g) g :filename "g2.png"
+(viz/save-graph (keys g) g :filename "g02.png"
                            :node->descriptor (fn [n] {:label n}))
 
 ; graf, kde i uzly existuji ve forme zaznamu
@@ -24,7 +24,7 @@
         "ANSI C" nil})
 
 ; ulozeni grafu i s popisky uzlu
-(viz/save-graph (keys g) g :filename "g3.png"
+(viz/save-graph (keys g) g :filename "g03.png"
                            :node->descriptor (fn [n] {:label n}))
 
 ; slozitejsi graf, kde maji uzly vice potomku
@@ -39,21 +39,21 @@
         "C99" nil})
 
 ; ulozeni grafu i s popisky uzlu
-(viz/save-graph (keys g) g :filename "g4.png"
+(viz/save-graph (keys g) g :filename "g04.png"
                            :node->descriptor (fn [n] {:label n}))
 
 ; zmena stylu vykresleni grafu - bez orientovanych hran
-(viz/save-graph (keys g) g :filename "g5.png"
+(viz/save-graph (keys g) g :filename "g05.png"
                            :node->descriptor (fn [n] {:label n})
                            :directed? nil)
 
 ; zmena stylu vykresleni grafu - horizontalne orientovany
-(viz/save-graph (keys g) g :filename "g6.png"
+(viz/save-graph (keys g) g :filename "g06.png"
                            :node->descriptor (fn [n] {:label n})
                            :vertical? false)
 
 ; ulozeni grafu i s popisky uzlu a hran
-(viz/save-graph (keys g) g :filename "g7.png"
+(viz/save-graph (keys g) g :filename "g07.png"
                            :node->descriptor (fn [n] {:label n})
                            :edge->descriptor (fn [n1 n2] {:label (str n1 "&rarr;" n2)}))
 
@@ -67,7 +67,29 @@
 
 ; dtto ovsem bez pouziti anonymnich funkci
 ; ulozeni grafu i s popisky uzlu a hran
-(viz/save-graph (keys g) g :filename "g8.png"
+(viz/save-graph (keys g) g :filename "g08.png"
                            :node->descriptor make-node-descriptor
                            :edge->descriptor make-edge-descriptor)
 
+(defn make-node-descriptor-2
+  [n]
+  {:label n
+   :color (if (= n "ANSI C") "darkgreen" "#000000")})
+
+; styly vykresleni uzlu
+(viz/save-graph (keys g) g :filename "g09.png"
+                           :node->descriptor make-node-descriptor-2
+                           :edge->descriptor make-edge-descriptor)
+
+(defn make-edge-descriptor-2
+  [n1 n2]
+  (let [color (cond (= n1 "ANSI C") "red"
+                    (= n2 "ANSI C") "blue"
+                    :else "black")]
+    {:label (str n1 "&rarr;" n2)
+     :color color}))
+
+; styly vykresleni hran
+(viz/save-graph (keys g) g :filename "g10.png"
+                           :node->descriptor make-node-descriptor-2
+                           :edge->descriptor make-edge-descriptor-2)
