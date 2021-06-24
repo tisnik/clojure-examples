50760627c77d996f4162b3252b638a66856c0a15
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-06-24 13:54:36 +0200

    New project: kafka-repl

---
+++ kafka-repl/project.clj
@@ -0,0 +1,25 @@
+;
+;  (C) Copyright 2020  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
+(defproject kafka-repl "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [fundingcircle/jackdaw "0.7.6"]]
+  :plugins [[lein-kibit "0.1.8"]
+            [lein-marginalia "0.9.1"]]
+  :main ^:skip-aot kafka-repl.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
