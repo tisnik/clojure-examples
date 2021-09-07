f864743c3af138c1ddfb633a80ab6ec550ec1bcb
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-07 12:20:10 +0200

    Added lein-codox into list of plugins

--- clj-hello/project.clj
+++ clj-hello/project.clj
@@ -15,4 +15,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]])
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :plugins [[lein-codox "0.10.7"]
+            ]
+)
