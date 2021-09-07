d5d01e9b29db9b75c28d09e8e0dfb064f9c7422d
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-07 12:20:10 +0200

    Added lein-codox into list of plugins

--- dis/project.clj
+++ dis/project.clj
@@ -16,7 +16,9 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
-  :plugins [[lein-nodisassemble "0.1.3"]]
+  :plugins [[lein-codox "0.10.7"]
+            [lein-nodisassemble "0.1.3"]
+            ]
   :main ^:skip-aot dis.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
