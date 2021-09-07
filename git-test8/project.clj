c53e17cf13a03f82307b5725bffe84a35944b580
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-07 11:54:16 +0200

    Reformatted project file

--- git-test8/project.clj
+++ git-test8/project.clj
@@ -11,13 +11,13 @@
 ;
 
 (defproject git-test8 "0.1.0-SNAPSHOT"
-    :description "FIXME: write description"
-    :url "http://example.com/FIXME"
-    :license {:name "Eclipse Public License"
-              :url "http://www.eclipse.org/legal/epl-v10.html"}
-    :dependencies [[org.clojure/clojure "1.10.1"]
-                   [clj-jgit "0.8.0"]
-                   [clj-rm-rf "1.0.0-SNAPSHOT"]]
-    :main ^:skip-aot git-test8.core
-    :target-path "target/%s"
-    :profiles {:uberjar {:aot :all}})
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [clj-jgit "0.8.0"]
+                 [clj-rm-rf "1.0.0-SNAPSHOT"]]
+  :main ^:skip-aot git-test8.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
