9a79047b97f82215404633d8d3c9535a5eafa017
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-07 12:20:10 +0200

    Added lein-codox into list of plugins

--- conditions/project.clj
+++ conditions/project.clj
@@ -16,7 +16,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
-  :plugins [[lein-marginalia "0.9.1"]]
+  :plugins [[codox "0.8.11"]
+            ]
   :main ^:skip-aot conditions.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
