424e5c27f9ca14bbe16548f5563408d6d38c9afd
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-07 12:20:10 +0200

    Added lein-codox into list of plugins

--- codoxtest/project.clj
+++ codoxtest/project.clj
@@ -16,7 +16,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
+  :plugins [[codox "0.8.11"]
+            ]
   :main ^:skip-aot codoxtest.core
   :target-path "target/%s"
-  :plugins [[codox "0.8.11"]]
   :profiles {:uberjar {:aot :all}})
