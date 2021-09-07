c8b971db6109153873992e05c3993a6816420c81
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-07 12:20:10 +0200

    Added lein-codox into list of plugins

--- clojupyter-project/project.clj
+++ clojupyter-project/project.clj
@@ -17,6 +17,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [incanter "1.9.3"]]
+  :plugins [[lein-codox "0.10.7"]
+            ]
   :main ^:skip-aot xyz.core
   :target-path "target/%s"
   :plugins [[lein-jupyter "0.1.16"]]
