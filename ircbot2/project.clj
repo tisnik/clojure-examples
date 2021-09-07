510d13be752ea1078d255ef6d004c120f47dea21
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-07 12:20:10 +0200

    Added lein-codox into list of plugins

--- ircbot2/project.clj
+++ ircbot2/project.clj
@@ -17,6 +17,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [irclj "0.5.0-alpha4"]]
+  :plugins [[lein-codox "0.10.7"]
+            ]
   :main ^:skip-aot ircbot2.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
