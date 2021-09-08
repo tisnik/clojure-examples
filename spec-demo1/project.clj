a66fc5f8acd606b80d251b1a58590512a94f05dd
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-08 07:51:28 +0200

    Added lein-codox into list of plugins

--- spec-demo1/project.clj
+++ spec-demo1/project.clj
@@ -11,11 +11,13 @@
 ;
 
 (defproject spec-demo1 "0.1.0-SNAPSHOT"
-    :description "FIXME: write description"
-    :url "http://example.com/FIXME"
-    :license {:name "Eclipse Public License"
-              :url "http://www.eclipse.org/legal/epl-v10.html"}
-    :dependencies [[org.clojure/clojure "1.10.1"]]
-    :main ^:skip-aot spec-demo1.core
-    :target-path "target/%s"
-    :profiles {:uberjar {:aot :all}})
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :plugins [[lein-codox "0.10.7"]
+            ]
+  :main ^:skip-aot spec-demo1.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
