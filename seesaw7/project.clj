f05424e207468a0c450432623ce2f59ffc62086f
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-08 07:51:28 +0200

    Added lein-codox into list of plugins

--- seesaw7/project.clj
+++ seesaw7/project.clj
@@ -17,6 +17,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [seesaw "1.4.5"]]
+  :plugins [[lein-codox "0.10.7"]
+            ]
   :main ^:skip-aot seesaw7.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
