8c99fe6e77dbb73ea48c26db6d8af484bd103ee5
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-09 08:15:08 +0200

    Added a new dependency: lein-codox

--- spec-demo5/project.clj
+++ spec-demo5/project.clj
@@ -16,6 +16,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
+  :plugins [[lein-codox "0.10.7"]
+            ]
   :main ^:skip-aot spec-demo5.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
