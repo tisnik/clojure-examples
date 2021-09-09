2e9a1d441d852d2a24a5179824d7b8673cd6887b
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-09 08:09:58 +0200

    Reformatted project file

--- db-store/project.clj
+++ db-store/project.clj
@@ -11,13 +11,13 @@
 ;
 
 (defproject db-store "0.1.0-SNAPSHOT"
-    :description "FIXME: write description"
-    :url "http://example.com/FIXME"
-    :license {:name "Eclipse Public License"
-              :url "http://www.eclipse.org/legal/epl-v10.html"}
-    :dependencies [[org.clojure/clojure "1.9.0"]
-                   [org.clojure/java.jdbc "0.3.5"]
-                   [org.xerial/sqlite-jdbc "3.7.2"]]
-    :main ^:skip-aot db-store.core
-    :target-path "target/%s"
-    :profiles {:uberjar {:aot :all}})
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/java.jdbc "0.3.5"]
+                 [org.xerial/sqlite-jdbc "3.7.2"]]
+  :main ^:skip-aot db-store.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
