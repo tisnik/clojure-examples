d1ddd19da3ca1b3877706e1034e5bf927a0bf983
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-09 07:45:08 +0200

    Reformatted project file

--- enlive4/project.clj
+++ enlive4/project.clj
@@ -11,13 +11,13 @@
 ;
 
 (defproject enlive4 "0.1.0-SNAPSHOT"
-    :description "FIXME: write description"
-    :url "http://example.com/FIXME"
-    :license {:name "Eclipse Public License"
-              :url "http://www.eclipse.org/legal/epl-v10.html"}
-    :dependencies [[org.clojure/clojure "1.10.1"]
-                   [enlive "1.1.1"]]
-    :main ^:skip-aot enlive4.core
-    :target-path "target/%s"
-    :profiles {:uberjar {:aot :all}})
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [enlive "1.1.1"]]
+  :main ^:skip-aot enlive4.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
 
