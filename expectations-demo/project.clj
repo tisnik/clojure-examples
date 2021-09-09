6f654cb0d566ac7b63ba5563879aed3cb2f4c0c9
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-09 08:10:20 +0200

    Reformatted project file

--- expectations-demo/project.clj
+++ expectations-demo/project.clj
@@ -11,13 +11,13 @@
 ;
 
 (defproject expectations-demo "0.1.0-SNAPSHOT"
-    :description "FIXME: write description"
-    :url "http://example.com/FIXME"
-    :license {:name "Eclipse Public License"
-              :url "http://www.eclipse.org/legal/epl-v10.html"}
-    :dependencies [[org.clojure/clojure "1.10.1"]
-                   [expectations "2.0.9"]]
-    :main ^:skip-aot expectations-demo.core
-    :target-path "target/%s"
-    :plugins [[lein-expectations "0.0.8"]]
-    :profiles {:uberjar {:aot :all}})
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [expectations "2.0.9"]]
+  :main ^:skip-aot expectations-demo.core
+  :target-path "target/%s"
+  :plugins [[lein-expectations "0.0.8"]]
+  :profiles {:uberjar {:aot :all}})
