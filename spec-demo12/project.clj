b9232a22c9d26f9145e9969d0425d2a445176ba6
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-09 07:45:08 +0200

    Reformatted project file

--- spec-demo12/project.clj
+++ spec-demo12/project.clj
@@ -11,12 +11,12 @@
 ;
 
 (defproject spec-demo12 "0.1.0-SNAPSHOT"
-    :description "FIXME: write description"
-    :url "http://example.com/FIXME"
-    :license {:name "Eclipse Public License"
-              :url "http://www.eclipse.org/legal/epl-v10.html"}
-    :dependencies [[org.clojure/clojure "1.10.1"]]
-    :main ^:skip-aot spec-demo12.core
-    :target-path "target/%s"
-    :profiles {:uberjar {:aot :all}
-               :dev {:dependencies [[org.clojure/test.check "0.9.0"]]}})
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :main ^:skip-aot spec-demo12.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}
+             :dev {:dependencies [[org.clojure/test.check "0.9.0"]]}})
