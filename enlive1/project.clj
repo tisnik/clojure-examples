9fbba3060728582ef6d48ea5f0787e1e2876e40c
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-24 08:05:55 +0200

    Added new dependency: test2junit

--- enlive1/project.clj
+++ enlive1/project.clj
@@ -18,6 +18,7 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [enlive "1.1.1"]]
   :plugins [[lein-codox "0.10.7"]
+            [test2junit "1.1.0"]
             ]
   :main ^:skip-aot enlive1.core
   :target-path "target/%s"
