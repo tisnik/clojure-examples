8e9d49de21f6b1abaf2971ae4c0bfdacec50a1cf
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-24 08:05:55 +0200

    Added new dependency: test2junit

--- htmltest3/project.clj
+++ htmltest3/project.clj
@@ -18,6 +18,7 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [hiccup "1.0.4"]]
   :plugins [[lein-codox "0.10.7"]
+            [test2junit "1.1.0"]
             ]
   :main ^:skip-aot htmltest3.core
   :target-path "target/%s"
