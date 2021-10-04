22090841c1556453c5e7c79adae0c769a3a31ae8
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-10-04 09:00:35 +0200

    Added new dependency: test2junit

--- test-clj-flotr/project.clj
+++ test-clj-flotr/project.clj
@@ -18,6 +18,7 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojars.tisnik/clj-flotr "0.1.0-SNAPSHOT"]]
   :plugins [[lein-codox "0.10.7"]
+            [test2junit "1.1.0"]
             ]
   :main ^:skip-aot test-clj-flotr.core
   :target-path "target/%s"
