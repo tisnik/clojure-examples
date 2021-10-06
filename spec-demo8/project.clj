955c1724c94c9edc933c3ee9c5b285b02c0fe612
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-10-06 08:24:18 +0200

    Added new dependency lein-cloverage

--- spec-demo8/project.clj
+++ spec-demo8/project.clj
@@ -18,6 +18,8 @@
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :plugins [[lein-codox "0.10.7"]
             [test2junit "1.1.0"]
+            ;[lein-test-out "0.3.1"]
+            [lein-cloverage "1.0.7-SNAPSHOT"]
             ]
   :main ^:skip-aot spec-demo8.core
   :target-path "target/%s"
