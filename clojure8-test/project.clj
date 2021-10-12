984c14ea648370f759e49008268e3fb312c9c88f
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-10-12 08:06:57 +0200

    Added new dependency - lein kibit

--- clojure8-test/project.clj
+++ clojure8-test/project.clj
@@ -20,6 +20,7 @@
             [test2junit "1.1.0"]
             ;[lein-test-out "0.3.1"]
             [lein-cloverage "1.0.7-SNAPSHOT"]
+            [lein-kibit "0.1.8"]
             ]
   :main ^:skip-aot clojure8-test.core
   :target-path "target/%s"
