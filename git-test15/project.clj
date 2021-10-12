887287dc1359d0b3af2680377e525a0a4798eaa4
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-10-12 08:06:58 +0200

    Added new dependency - lein kibit

--- git-test15/project.clj
+++ git-test15/project.clj
@@ -22,6 +22,7 @@
             [test2junit "1.1.0"]
             ;[lein-test-out "0.3.1"]
             [lein-cloverage "1.0.7-SNAPSHOT"]
+            [lein-kibit "0.1.8"]
             ]
   :main ^:skip-aot git-test15.core
   :target-path "target/%s"
