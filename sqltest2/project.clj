85b20b48b1ef214a93eac4e37c0d8804d285e74d
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-05 07:34:20 +0100

    Added new dependency to project file: lein-clean-m2

--- sqltest2/project.clj
+++ sqltest2/project.clj
@@ -23,6 +23,7 @@
             ;[lein-test-out "0.3.1"]
             [lein-cloverage "1.0.7-SNAPSHOT"]
             [lein-kibit "0.1.8"]
+            [lein-clean-m2 "0.1.2"]
             ]
   :main ^:skip-aot sqltest2.core
   :target-path "target/%s"
