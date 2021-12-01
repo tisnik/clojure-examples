76eea1132fa6a44df7d64bc0e54ec02ec68ef317
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-12-01 07:21:21 +0100

    Added new dependency: lein-project-edn

--- cucumber+expectB/project.clj
+++ cucumber+expectB/project.clj
@@ -24,6 +24,7 @@
             [lein-cloverage "1.0.7-SNAPSHOT"]
             [lein-kibit "0.1.8"]
             [lein-clean-m2 "0.1.2"]
+            [lein-project-edn "0.3.0"]
             [com.siili/lein-cucumber "1.0.7"]
             [lein-expectations "0.0.8"]
             ]
