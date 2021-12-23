654efe8764adac9cc5562c7f785e6d2374ad605c
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-12-23 10:26:17 +0100

    Fixed project settings

--- spec-demo6/project.clj
+++ spec-demo6/project.clj
@@ -24,7 +24,6 @@
             [lein-clean-m2 "0.1.2"]
             [lein-project-edn "0.3.0"]
             [lein-marginalia "0.9.1"]]
-            ]
   :main ^:skip-aot spec-demo6.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
