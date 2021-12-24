e640e27f7147902e43e04324a5bfe6b5e00979ff
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-12-24 08:29:22 +0100

    Fixed project settings

--- git-test4/project.clj
+++ git-test4/project.clj
@@ -26,7 +26,6 @@
             [lein-clean-m2 "0.1.2"]
             [lein-project-edn "0.3.0"]
             [lein-marginalia "0.9.1"]]
-            ]
   :main ^:skip-aot git-test4.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
