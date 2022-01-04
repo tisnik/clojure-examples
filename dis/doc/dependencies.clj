aae44cea92cece1c36e41dd68f99ead23de6a56a
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-04 09:26:10 +0100

    Dependency list for this project

---
+++ dis/doc/dependencies.clj
@@ -0,0 +1,35 @@
+{[clojure-complete "0.2.5" :exclusions [[org.clojure/clojure]]] nil,
+ [nodisassemble "0.1.3"]
+ {[org.eclipse.jdt/org.eclipse.jdt.core "3.7.1"]
+  {[org.eclipse.core/org.eclipse.core.filesystem "1.3.100"] nil,
+   [org.eclipse.core/org.eclipse.core.resources "3.7.100"]
+   {[org.eclipse.core/org.eclipse.core.expressions "3.4.300"] nil},
+   [org.eclipse.core/org.eclipse.core.runtime "3.7.0"]
+   {[org.eclipse.core/org.eclipse.core.contenttype "3.4.100"] nil,
+    [org.eclipse.core/org.eclipse.core.jobs "3.5.100"] nil,
+    [org.eclipse.equinox/org.eclipse.equinox.app "1.3.100"] nil,
+    [org.eclipse.equinox/org.eclipse.equinox.common "3.6.0"] nil,
+    [org.eclipse.equinox/org.eclipse.equinox.preferences "3.4.1"] nil,
+    [org.eclipse.equinox/org.eclipse.equinox.registry "3.5.101"] nil,
+    [org.eclipse.osgi "3.7.1"] nil},
+   [org.eclipse.text "3.5.101"]
+   {[org.eclipse.core/org.eclipse.core.commands "3.6.0"] nil}}},
+ [nrepl "0.7.0" :exclusions [[org.clojure/clojure]]] nil,
+ [org.clojure/clojure "1.10.1"]
+ {[org.clojure/core.specs.alpha "0.2.44"] nil,
+  [org.clojure/spec.alpha "0.2.176"] nil},
+ [venantius/ultra "0.6.0"]
+ {[grimradical/clj-semver "0.3.0" :exclusions [[org.clojure/clojure]]]
+  nil,
+  [io.aviso/pretty "0.1.35"] nil,
+  [mvxcvi/puget "1.1.0"]
+  {[fipp "0.6.14"] {[org.clojure/core.rrb-vector "0.0.13"] nil},
+   [mvxcvi/arrangement "1.1.1"] nil},
+  [mvxcvi/whidbey "2.1.0"] {[org.clojure/data.codec "0.1.1"] nil},
+  [org.clojars.brenton/google-diff-match-patch "0.1"] nil,
+  [robert/hooke "1.3.0"] nil,
+  [venantius/glow "0.1.5" :exclusions [[hiccup] [garden]]]
+  {[clj-antlr "0.2.3"]
+   {[org.antlr/antlr4-runtime "4.5.3"] nil,
+    [org.antlr/antlr4 "4.5.3"] nil},
+   [instaparse "1.4.1"] nil}}}
