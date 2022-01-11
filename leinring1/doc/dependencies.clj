0f050e5057907bc8e4656cae57083e965dfc30e4
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-11 09:47:17 +0100

    Dependency list for this project

---
+++ leinring1/doc/dependencies.clj
@@ -0,0 +1,38 @@
+{[clojure-complete "0.2.5" :exclusions [[org.clojure/clojure]]] nil,
+ [hiccup "1.0.4"] nil,
+ [nrepl "0.7.0" :exclusions [[org.clojure/clojure]]] nil,
+ [org.clojure/clojure "1.10.1"]
+ {[org.clojure/core.specs.alpha "0.2.44"] nil,
+  [org.clojure/spec.alpha "0.2.176"] nil},
+ [org.clojure/tools.cli "0.3.1"] nil,
+ [ring/ring-core "1.3.2"]
+ {[clj-time "0.6.0"] {[joda-time "2.2"] nil},
+  [commons-fileupload "1.3"] nil,
+  [commons-io "2.4"] nil,
+  [crypto-equality "1.0.0"] nil,
+  [crypto-random "1.2.0"] nil,
+  [org.clojure/tools.reader "0.8.1"] nil,
+  [ring/ring-codec "1.0.0"] {[commons-codec "1.6"] nil}},
+ [ring/ring-jetty-adapter "1.3.2"]
+ {[org.eclipse.jetty/jetty-server "7.6.13.v20130916"]
+  {[org.eclipse.jetty.orbit/javax.servlet "2.5.0.v201103041518"] nil,
+   [org.eclipse.jetty/jetty-continuation "7.6.13.v20130916"] nil,
+   [org.eclipse.jetty/jetty-http "7.6.13.v20130916"]
+   {[org.eclipse.jetty/jetty-io "7.6.13.v20130916"]
+    {[org.eclipse.jetty/jetty-util "7.6.13.v20130916"] nil}}},
+  [ring/ring-servlet "1.3.2"] nil},
+ [venantius/ultra "0.6.0"]
+ {[grimradical/clj-semver "0.3.0" :exclusions [[org.clojure/clojure]]]
+  nil,
+  [io.aviso/pretty "0.1.35"] nil,
+  [mvxcvi/puget "1.1.0"]
+  {[fipp "0.6.14"] {[org.clojure/core.rrb-vector "0.0.13"] nil},
+   [mvxcvi/arrangement "1.1.1"] nil},
+  [mvxcvi/whidbey "2.1.0"] {[org.clojure/data.codec "0.1.1"] nil},
+  [org.clojars.brenton/google-diff-match-patch "0.1"] nil,
+  [robert/hooke "1.3.0"] nil,
+  [venantius/glow "0.1.5" :exclusions [[hiccup] [garden]]]
+  {[clj-antlr "0.2.3"]
+   {[org.antlr/antlr4-runtime "4.5.3"] nil,
+    [org.antlr/antlr4 "4.5.3"] nil},
+   [instaparse "1.4.1"] nil}}}
