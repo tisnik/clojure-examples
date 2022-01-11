c1703a9ad100132602627c877e1c7df2dc07db75
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-11 09:47:17 +0100

    Dependency list for this project

---
+++ seesaw9/doc/dependencies.clj
@@ -0,0 +1,31 @@
+{[clojure-complete "0.2.5" :exclusions [[org.clojure/clojure]]] nil,
+ [nrepl "0.7.0" :exclusions [[org.clojure/clojure]]] nil,
+ [org.clojure/clojure "1.10.1"]
+ {[org.clojure/core.specs.alpha "0.2.44"] nil,
+  [org.clojure/spec.alpha "0.2.176"] nil},
+ [seesaw "1.4.5"]
+ {[com.fifesoft/rsyntaxtextarea "2.5.6"] nil,
+  [com.jgoodies/forms "1.2.1"] nil,
+  [com.miglayout/miglayout "3.7.4"] nil,
+  [j18n "1.0.2"] nil,
+  [org.swinglabs.swingx/swingx-core "1.6.3"]
+  {[org.swinglabs.swingx/swingx-action "1.6.3"] nil,
+   [org.swinglabs.swingx/swingx-autocomplete "1.6.3"]
+   {[org.swinglabs.swingx/swingx-common "1.6.3"] nil},
+   [org.swinglabs.swingx/swingx-painters "1.6.3"] nil,
+   [org.swinglabs.swingx/swingx-plaf "1.6.3"] nil}},
+ [venantius/ultra "0.6.0"]
+ {[grimradical/clj-semver "0.3.0" :exclusions [[org.clojure/clojure]]]
+  nil,
+  [io.aviso/pretty "0.1.35"] nil,
+  [mvxcvi/puget "1.1.0"]
+  {[fipp "0.6.14"] {[org.clojure/core.rrb-vector "0.0.13"] nil},
+   [mvxcvi/arrangement "1.1.1"] nil},
+  [mvxcvi/whidbey "2.1.0"] {[org.clojure/data.codec "0.1.1"] nil},
+  [org.clojars.brenton/google-diff-match-patch "0.1"] nil,
+  [robert/hooke "1.3.0"] nil,
+  [venantius/glow "0.1.5" :exclusions [[hiccup] [garden]]]
+  {[clj-antlr "0.2.3"]
+   {[org.antlr/antlr4-runtime "4.5.3"] nil,
+    [org.antlr/antlr4 "4.5.3"] nil},
+   [instaparse "1.4.1"] nil}}}
