8ec12b825b55fd265756db65364115f259ffce9a
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-11 09:47:17 +0100

    Dependency list for this project

---
+++ git-test9/doc/dependencies.clj
@@ -0,0 +1,34 @@
+{[clj-jgit "0.8.0"]
+ {[fs "1.3.2"] {[org.apache.commons/commons-compress "1.3"] nil},
+  [org.clojure/core.memoize "0.5.3"]
+  {[org.clojure/core.cache "0.6.3"]
+   {[org.clojure/data.priority-map "0.0.2"] nil}},
+  [org.eclipse.jgit/org.eclipse.jgit.java7 "3.5.0.201409260305-r"]
+  {[org.eclipse.jgit "3.5.0.201409260305-r"]
+   {[com.googlecode.javaewah/JavaEWAH "0.7.9"] nil,
+    [com.jcraft/jsch "0.1.50"] nil,
+    [org.apache.httpcomponents/httpclient "4.1.3"]
+    {[commons-codec "1.4"] nil,
+     [commons-logging "1.1.1"] nil,
+     [org.apache.httpcomponents/httpcore "4.1.4"] nil}}}},
+ [clj-rm-rf "1.0.0-20100821.141701-3"] nil,
+ [clojure-complete "0.2.5" :exclusions [[org.clojure/clojure]]] nil,
+ [nrepl "0.7.0" :exclusions [[org.clojure/clojure]]] nil,
+ [org.clojure/clojure "1.10.1"]
+ {[org.clojure/core.specs.alpha "0.2.44"] nil,
+  [org.clojure/spec.alpha "0.2.176"] nil},
+ [venantius/ultra "0.6.0"]
+ {[grimradical/clj-semver "0.3.0" :exclusions [[org.clojure/clojure]]]
+  nil,
+  [io.aviso/pretty "0.1.35"] nil,
+  [mvxcvi/puget "1.1.0"]
+  {[fipp "0.6.14"] {[org.clojure/core.rrb-vector "0.0.13"] nil},
+   [mvxcvi/arrangement "1.1.1"] nil},
+  [mvxcvi/whidbey "2.1.0"] {[org.clojure/data.codec "0.1.1"] nil},
+  [org.clojars.brenton/google-diff-match-patch "0.1"] nil,
+  [robert/hooke "1.3.0"] nil,
+  [venantius/glow "0.1.5" :exclusions [[hiccup] [garden]]]
+  {[clj-antlr "0.2.3"]
+   {[org.antlr/antlr4-runtime "4.5.3"] nil,
+    [org.antlr/antlr4 "4.5.3"] nil},
+   [instaparse "1.4.1"] nil}}}
