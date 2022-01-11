eaca9c742f48c6c53710193bbf21d6c9771a8fb8
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-11 09:47:16 +0100

    Dependency list for this project

---
+++ forest-demo/doc/dependencies.clj
@@ -0,0 +1,50 @@
+{[clojure-complete "0.2.5" :exclusions [[org.clojure/clojure]]] nil,
+ [nrepl "0.7.0" :exclusions [[org.clojure/clojure]]] nil,
+ [org.clojure/clojure "1.10.1"]
+ {[org.clojure/core.specs.alpha "0.2.44"] nil,
+  [org.clojure/spec.alpha "0.2.176"] nil},
+ [tupelo "21.04.13"]
+ {[cheshire "5.10.0"]
+  {[com.fasterxml.jackson.core/jackson-core "2.10.2"] nil,
+   [com.fasterxml.jackson.dataformat/jackson-dataformat-cbor "2.10.2"]
+   nil,
+   [com.fasterxml.jackson.dataformat/jackson-dataformat-smile "2.10.2"]
+   nil,
+   [tigris "0.1.2"] nil},
+  [clj-time "0.15.2"] nil,
+  [clojure-csv "2.0.2"] nil,
+  [danlentz/clj-uuid "0.1.9"] {[primitive-math "0.1.6"] nil},
+  [joda-time "2.10.10"] nil,
+  [org.ccil.cowan.tagsoup/tagsoup "1.2.1"] nil,
+  [org.clojure/core.async "1.3.610"]
+  {[org.clojure/tools.analyzer.jvm "1.1.0"]
+   {[org.clojure/core.memoize "1.0.236"]
+    {[org.clojure/core.cache "1.0.207"]
+     {[org.clojure/data.priority-map "1.0.0"] nil}},
+    [org.clojure/tools.analyzer "1.0.0"] nil,
+    [org.ow2.asm/asm "5.2"] nil}},
+  [org.clojure/core.match "1.0.0"] nil,
+  [org.clojure/data.avl "0.1.0"] nil,
+  [org.clojure/data.xml "0.2.0-alpha6"]
+  {[org.clojure/data.codec "0.1.0"] nil},
+  [org.clojure/math.combinatorics "0.1.6"] nil,
+  [org.clojure/test.check "1.1.0"] nil,
+  [org.clojure/tools.reader "1.3.5"] nil,
+  [org.flatland/ordered "1.5.9"] nil,
+  [org.snakeyaml/snakeyaml-engine "2.2.1"] nil,
+  [prismatic/schema "1.1.12"] nil},
+ [venantius/ultra "0.6.0"]
+ {[grimradical/clj-semver "0.3.0" :exclusions [[org.clojure/clojure]]]
+  nil,
+  [io.aviso/pretty "0.1.35"] nil,
+  [mvxcvi/puget "1.1.0"]
+  {[fipp "0.6.14"] {[org.clojure/core.rrb-vector "0.0.13"] nil},
+   [mvxcvi/arrangement "1.1.1"] nil},
+  [mvxcvi/whidbey "2.1.0"] nil,
+  [org.clojars.brenton/google-diff-match-patch "0.1"] nil,
+  [robert/hooke "1.3.0"] nil,
+  [venantius/glow "0.1.5" :exclusions [[hiccup] [garden]]]
+  {[clj-antlr "0.2.3"]
+   {[org.antlr/antlr4-runtime "4.5.3"] nil,
+    [org.antlr/antlr4 "4.5.3"] nil},
+   [instaparse "1.4.1"] nil}}}
