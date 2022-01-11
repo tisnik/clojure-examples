75ed7c7f37008a852cda56ec8c3a47ad21c80dcd
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-11 09:47:16 +0100

    Dependency list for this project

---
+++ enlive3/doc/dependencies.clj
@@ -0,0 +1,21 @@
+{[clojure-complete "0.2.5" :exclusions [[org.clojure/clojure]]] nil,
+ [enlive "1.1.1"] {[org.ccil.cowan.tagsoup/tagsoup "1.2.1"] nil},
+ [nrepl "0.7.0" :exclusions [[org.clojure/clojure]]] nil,
+ [org.clojure/clojure "1.10.1"]
+ {[org.clojure/core.specs.alpha "0.2.44"] nil,
+  [org.clojure/spec.alpha "0.2.176"] nil},
+ [venantius/ultra "0.6.0"]
+ {[grimradical/clj-semver "0.3.0" :exclusions [[org.clojure/clojure]]]
+  nil,
+  [io.aviso/pretty "0.1.35"] nil,
+  [mvxcvi/puget "1.1.0"]
+  {[fipp "0.6.14"] {[org.clojure/core.rrb-vector "0.0.13"] nil},
+   [mvxcvi/arrangement "1.1.1"] nil},
+  [mvxcvi/whidbey "2.1.0"] {[org.clojure/data.codec "0.1.1"] nil},
+  [org.clojars.brenton/google-diff-match-patch "0.1"] nil,
+  [robert/hooke "1.3.0"] nil,
+  [venantius/glow "0.1.5" :exclusions [[hiccup] [garden]]]
+  {[clj-antlr "0.2.3"]
+   {[org.antlr/antlr4-runtime "4.5.3"] nil,
+    [org.antlr/antlr4 "4.5.3"] nil},
+   [instaparse "1.4.1"] nil}}}
